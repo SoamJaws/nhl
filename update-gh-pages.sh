4e820c224d0c9629431bdffcd6df19fea7a18993
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-06-13 15:41:59 +0000

    Deploy SoamJaws/nhl to github.com/SoamJaws/nhl.git:gh-pages

---
+++ update-gh-pages.sh
@@ -0,0 +1,25 @@
+echo -e "Starting to update gh-pages\n"
+
+#copy data we're interested in to other place
+
+#go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+#using token clone gh-pages branch
+git clone https://${GH_TOKEN}@github.com/SoamJaws/nhl.git
+
+#go into directory and copy data we're interested in to that directory
+cd nhl
+git remote rm origin
+git remote add origin https://${GH_TOKEN}@github.com/SoamJaws/nhl.git
+git fetch
+git checkout -b gh-pages
+git rebase master
+./nhl html > index.html
+
+#add, commit and push files
+git add index.html
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git push origin gh-pages
