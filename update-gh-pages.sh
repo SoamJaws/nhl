262ff0ee5112895d42fb3f3cad25c3d26bc55530
Johan W-Schützer <johan.dss@gmail.com>
2017-06-15 17:20:56 +0200

    Update ghpages fix for unstaged changes

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -3,7 +3,15 @@
 git pull origin master
 
 HTML_CONTENT=$(./nhl html)
+STASHED=false
+
 git checkout gh-pages
+if [[ "$?" != 0 ]]; then
+	git stash
+	git checkout gh-pages
+	STASHED=true
+fi
+
 git pull origin gh-pages
 echo "$HTML_CONTENT" > index.html
 
@@ -12,3 +20,7 @@ git commit -m "Updated stats"
 
 git push origin gh-pages
 git checkout -
+
+if [[ "$STASHED" = true ]]; then
+	git stash pop -q
+fi
